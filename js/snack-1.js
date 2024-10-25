4241b954841cce26cbc55a82ea28279e6fb25d3e
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-25 15:21:29 +0200

    snack 1

--- js/snack-1.js
+++ js/snack-1.js
@@ -3,4 +3,32 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
 
-console.log("ok");
+//creo un array di oggetti
+const biciDaCorsa = [
+	//oggetto bici a
+	{
+		nome: "biciA",
+		peso: "1kg",
+	},
+	//oggetto bici b
+	{
+		nome: "biciB",
+		peso: "2kg",
+	},
+	//oggetto bici c
+	{
+		nome: "biciC",
+		peso: "3kg",
+	},
+	//oggetto bici d
+	{
+		nome: "biciD",
+		peso: "4kg",
+	},
+];
+
+//stampo l'array con tutti gli oggetti
+console.log(biciDaCorsa);
+
+//stampo l'oggetto bici con peso minore
+console.log(biciDaCorsa[0]);
