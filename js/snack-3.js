39cadc7382628cd2bd0863e74612514731fed22c
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-26 11:22:32 +0200

    snack 3

--- js/snack-3.js
+++ js/snack-3.js
@@ -2,4 +2,17 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 
-console.log("ok");
+const numberArray = [1, 10, 20, 30, 50, 75, 87, 93];
+
+const newArray = [];
+
+function myFunction(array, a, b) {
+	array.forEach((element) => {
+		if (element > a && element < b) {
+			newArray.push(element);
+		}
+	});
+}
+
+myFunction(numberArray, 1, 93);
+console.log(newArray);
